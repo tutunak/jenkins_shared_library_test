45bce6e4fdf88226ae741df7220907b59b3dc106
tutunak <dk@tutunak.com>
2021-03-10 22:15:56 +0300

    branch?

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,5 +1,5 @@
 def call(String name = 'human') {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
-    echo "Hello, ${BRANCH}."
+    echo "Hello, ${env.BRANCH}."
 }
